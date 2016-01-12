7bd1a87f120252cd9682d568eeae6bb73f6d12fd
sammme <samyak.upadhyay123@gmail.com>
2016-01-12 22:50:24 +0530

    adding 404 page and get object in views

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
@@ -8,10 +8,12 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
+    instance = get_object_or_404(Post, id =1)
     context = {
-        "title" : "Detail"
+        "title" : instance.title,
+        "instance": instance
      }
-    return render(request, "index.html", context)
+    return render(request, "post_detail.html", context)
 
 def post_list(request):
     #return HttpResponse("<h1>List</h1>")
@@ -20,6 +22,7 @@ def post_list(request):
     #        "title" : "My user List"
     #    }
     #else :
+
     queryset = Post.objects.all()
     context = {
         "object_list" : queryset,
