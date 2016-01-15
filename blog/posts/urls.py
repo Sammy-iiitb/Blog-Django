26167754abf7dd25f2d04ebde38b4dd4bf61c615
sammme <samyak.upadhyay123@gmail.com>
2016-01-15 23:12:19 +0530

    modified messages part and added delete form

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -5,10 +5,10 @@ from .views import post_list, post_create, post_detail, post_update, post_delete
 
 
 urlpatterns = [
-    url(r'^$', post_list),
+    url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
-    url(r'^delete/$', post_delete),
+    url(r'^(?P<id>\d+)/delete/$', post_delete),
     #url(r'^admin/', <app_name>.views.<function_view>),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,6 @@
-from djano.contrib import messages
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 # Create your views here.
 from .models import Post
@@ -11,10 +11,10 @@ def post_create(request):
         instance = form.save(commit = False)
         instance.save()
         #mesage success
-        message.success(request, "Successfully Created")
+        messages.success(request, "Successfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
     else :
-        return message.error(request, "Not Successfully Created")
+        return messages.error(request, "Not Successfully Created")
     #if request.method = "POST"
     #    print request.POST.get("content")
     # setting context to post_form.html
@@ -52,7 +52,7 @@ def post_update(request, id=None):
     if form.is_valid():
         instance = form.save(commit = False)
         instance.save()
-        message.success(request, "Item Saved", extra_tags='some-tag')
+        messages.success(request, "Item Saved", extra_tags='some-tag')
     return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "title" : instance.title,
@@ -61,5 +61,8 @@ def post_update(request, id=None):
      }
     return render(request, "post_form.html", context)
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id =id)
+    instance.delete()
+    messages.success(request, "Successfully Deleted")
+    return redirect("posts:list")
